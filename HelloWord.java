e32e567f580406927d6c1aa3942883eeca06e13d
Rakesh Prajapat <67090667+rakepraj@users.noreply.github.com>
2020-06-22 17:52:11 +0530

    Added java file

---
+++ HelloWord.java
@@ -0,0 +1,8 @@
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello,New World on 10th June. 14:37 hrs. 2020");
+  }
+}
